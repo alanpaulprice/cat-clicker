c3e56f8cd65120ad093054b2083592469e4f59a1
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2017-11-19 19:45:04 +0000

    updated to meet requirements 2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
-const catPic = document.getElementById("cat-pic");
-const countReadout = document.getElementById("count-readout");
-let count = 0;
+const
+catPic1 = document.getElementById("cat-pic-1"),
+catPic2 = document.getElementById("cat-pic-2"),
+clickCountReadout1 = document.getElementById("count-readout-1"),
+clickCountReadout2 = document.getElementById("count-readout-2");
 
-catPic.addEventListener("click", () => {
-  count++;
-  countReadout.textContent = count;
+let
+count1 = 0,
+count2 = 0;
+
+catPic1.addEventListener("click", () => {
+  count1++;
+  clickCountReadout1.textContent = count1;
+});
+
+catPic2.addEventListener("click", () => {
+  count2++;
+  clickCountReadout2.textContent = count2;
 });
